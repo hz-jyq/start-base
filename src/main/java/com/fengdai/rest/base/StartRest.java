bd0889682c3876668e0b66ba462a07cf68872b99
jinyuanqian <hzjyq@tairanchina.com>
2016-12-20 14:23:26 +0800

    add

---
+++ src/main/java/com/fengdai/rest/base/StartRest.java
@@ -0,0 +1,39 @@
+package com.fengdai.rest.base;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public abstract class StartRest {
+	
+	protected ResourceConfig buildResourceConfig() {
+		 	ResourceConfig rc =new ResourceConfig();
+		 	settingResourceConfig(rc);
+		 	return rc;
+	}
+	
+	public abstract ResourceConfig settingResourceConfig(ResourceConfig resourceConfig);
+	
+	
+	public HttpServer startServer(String baseUri) {
+	        System.out.println("Starting grizzly...");  
+	    	ResourceConfig rc = buildResourceConfig();
+	        HttpServer server  = GrizzlyHttpServerFactory.createHttpServer(URI.create(baseUri), rc);
+	        System.out.println(String.format("Jersey app started with WADL available at" + "%sapplication.wadl\nTry out %shelloworld\nHit enter to stop it...",baseUri,baseUri));
+	        System.out.println("...started");  
+	        try {
+				System.in.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	        server.shutdown();  
+	        System.exit(0);  
+	        return server;
+	}
+
+	
+	
+}
